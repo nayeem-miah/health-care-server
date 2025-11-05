536c529a4e2c632fa4742d946213d83e4f30dc10
nayeem-miah <nayeem5113a@gmail.com>
2025-11-05 10:43:07 +0600

    initaitl server

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+
+const app: Application = express();
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+        success: true,
+        message: "Ph health care server is running..",
+        status: 200,
+        timeStamp: new Date().toISOString()
+    })
+});
+
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
+
+export default app;
